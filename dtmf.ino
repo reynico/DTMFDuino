16485c6a92eafb5cc86eede5ab027e681fac1146
Nico Rey <rey.nico@gmail.com>
2013-01-30 00:26:51 -0300

    First beta release

--- dtmf.ino
+++ dtmf.ino
@@ -6,22 +6,49 @@
 	rey.nico@gmail.com
 */
 
-int bin0 = 6;
-int bin1 = 7;
-int bin2 = 8;
-int bin3 = 9;
-int STD  = 10;
+int bin0 = A4;
+int bin1 = A3;
+int bin2 = A2;
+int bin3 = A1;
+int STD  = A0;
+
+int Out1 =  2;
+int Out2 =  3;
+int Out3 =  4;
+int Out4 =  5;
+int Out5 =  6;
+int Out6 =  7;
+int Out7 =  8;
+int Out8 =  9;
+int Out9 =  10;
+int Out10 = 11;
+int checkOff = 12;
+int checkOn =  13;
+
+int i    = 1;
 char dtmf[5];
-int i = 1;
+
 byte irqState;
 
 void setup() {
 	Serial.begin(9600);
-	pinMode(bin0, INPUT);
-	pinMode(bin1, INPUT);
-	pinMode(bin2, INPUT);
-	pinMode(bin3, INPUT);
-	pinMode(STD,  INPUT);
+	pinMode(bin0,  INPUT);
+	pinMode(bin1,  INPUT);
+	pinMode(bin2,  INPUT);
+	pinMode(bin3,  INPUT);
+	pinMode(STD,   INPUT);
+	pinMode(Out1,  OUTPUT);
+	pinMode(Out2,  OUTPUT);
+	pinMode(Out3,  OUTPUT);
+	pinMode(Out4,  OUTPUT);
+	pinMode(Out5,  OUTPUT);
+	pinMode(Out6,  OUTPUT);
+	pinMode(Out7,  OUTPUT);
+	pinMode(Out8,  OUTPUT);
+	pinMode(Out9,  OUTPUT);
+	pinMode(Out10, OUTPUT);
+	pinMode(checkOff, OUTPUT);
+	pinMode(checkOn, OUTPUT);
 }
 
 void loop() {
@@ -35,22 +62,224 @@ void loop() {
 		delay(100); //delay to sync the STD data
 	} 
 	if (i == 4) { //When you reach the last digit..
-		i = 0;  //Resets the read counter
-		showCommand(); //And check the received data
+		i = 1;  //Resets the read counter	
+		controller();
 	}
 }
 
-void showCommand() {
-	if (dtmf[0] == '*') {
-	Serial.print("HOLA ");
-		if (dtmf[1] == '1') {
-			Serial.print("KE ");
-			if (dtmf[2] == '1') {
-				Serial.print("ASE");
-			} else {i = 0;}
-		} else {i = 0;}
-	} else {i = 0;}
-	i = 0; //Reset the read counter
+int getState(int checkPin) {
+// Check the output state from each pin
+	int out;
+	switch (checkPin) {
+		case 2:
+			out = bitRead(PORTD,2);
+		break;
+		case 3:
+			out = bitRead(PORTD,3);
+		break;
+		case 4:
+			out = bitRead(PORTD,4);
+		break;
+		case 5:
+			out = bitRead(PORTD,5);
+		break;
+		case 6:
+			out = bitRead(PORTD,6);
+		break;
+		case 7:
+			out = bitRead(PORTD,7);
+		break;
+		case 8:
+			out = bitRead(PORTB,0);
+		break;
+		case 9:
+			out = bitRead(PORTB,1);
+		break;
+		case 10:
+			out = bitRead(PORTB,2);
+		break;
+	}
+
+	if (out == 0) {
+		digitalWrite(checkOff,HIGH);
+		delay(500);
+		digitalWrite(checkOff,LOW);
+	} else {
+		digitalWrite(checkOn,HIGH);
+		delay(500);
+		digitalWrite(checkOn,LOW);
+	}
+	return out;
+}
+
+void controller () {
+  Serial.println(" ");
+	//Enable or disable outputs
+	if (dtmf[1] == '*') { //First-enabler tone
+		Serial.println("First char");
+		if (dtmf[2] == '0') { //Disable output
+			switch (dtmf[3]) {
+				case '1':
+					digitalWrite(Out1,LOW);
+					Serial.println("Out1 off");
+					break;
+				case '2':
+					digitalWrite(Out2,LOW);
+					Serial.println("Out2 off");
+					break;
+				case '3':
+					digitalWrite(Out3,LOW);
+					Serial.println("Out3 off");
+					break;
+				case '4':
+					digitalWrite(Out4,LOW);
+					Serial.println("Out4 off");
+					break;
+				case '5':
+					digitalWrite(Out5,LOW);
+					Serial.println("Out5 off");
+					break;
+				case '6':
+					digitalWrite(Out6,LOW);
+					Serial.println("Out6 off");
+					break;
+				case '7':
+					digitalWrite(Out7,LOW);
+					Serial.println("Out7 off");
+					break;
+				case '8':
+					digitalWrite(Out8,LOW);
+					Serial.println("Out8 off");
+					break;
+				case '9':
+					digitalWrite(Out9,LOW);
+					Serial.println("Out9 off");
+					break;
+				case 'A':
+					digitalWrite(Out10,LOW);
+					Serial.println("Out10 off");
+					break;
+			}
+		}
+		if (dtmf[2] == '1') { //Enable output
+			switch (dtmf[3]) {
+				case '1':
+					digitalWrite(Out1,HIGH);
+					Serial.println("Out1 on");
+					break;
+				case '2':
+					digitalWrite(Out2,HIGH);
+					Serial.println("Out2 on");
+					break;
+				case '3':
+					digitalWrite(Out3,HIGH);
+					Serial.println("Out3 on");
+					break;
+				case '4':
+					digitalWrite(Out4,HIGH);
+					Serial.println("Out4 on");
+					break;
+				case '5':
+					digitalWrite(Out5,HIGH);
+					Serial.println("Out5 on");
+					break;
+				case '6':
+					digitalWrite(Out6,HIGH);
+					Serial.println("Out6 on");
+					break;
+				case '7':
+					digitalWrite(Out7,HIGH);
+					Serial.println("Out7 on");
+					break;
+				case '8':
+					digitalWrite(Out8,HIGH);
+					Serial.println("Out8 on");
+					break;
+				case '9':
+					digitalWrite(Out9,HIGH);
+					Serial.println("Out9 on");
+					break;
+				case 'A':
+					digitalWrite(Out10,HIGH);
+					Serial.println("Out10 on");
+					break;
+			}
+		}
+		if (dtmf[2] == '2') { //Check output
+			switch (dtmf[3]) {
+				case '1':
+					if(getState(Out1) == 0) {
+						Serial.println("Out1 is off");
+					} else {
+						Serial.println("Out1 is on");
+					}
+					break;
+				case '2':
+					if(getState(Out2) == 0) {
+						Serial.println("Out2 is off");
+					} else {
+						Serial.println("Out2 is on");
+					}
+					break;
+				case '3':
+					if(getState(Out3) == 0) {
+						Serial.println("Out3 is off");
+					} else {
+						Serial.println("Out3 is on");
+					}
+					break;
+				case '4':
+					if(getState(Out4) == 0) {
+						Serial.println("Out4 is off");
+					} else {
+						Serial.println("Out4 is on");
+					}
+					break;
+				case '5':
+					if(getState(Out5) == 0) {
+						Serial.println("Out5 is off");
+					} else {
+						Serial.println("Out5 is on");
+					}
+					break;
+				case '6':
+					if(getState(Out6) == 0) {
+						Serial.println("Out6 is off");
+					} else {
+						Serial.println("Out6 is on");
+					}
+					break;
+				case '7':
+					if(getState(Out7) == 0) {
+						Serial.println("Out7 is off");
+					} else {
+						Serial.println("Out7 is on");
+					}
+					break;
+				case '8':
+					if(getState(Out8) == 0) {
+						Serial.println("Out8 is off");
+					} else {
+						Serial.println("Out8 is on");
+					}
+					break;
+				case '9':
+					if(getState(Out9) == 0) {
+						Serial.println("Out9 is off");
+					} else {
+						Serial.println("Out9 is on");
+					}
+					break;
+				case 'A':
+					if(getState(Out10) == 0) {
+						Serial.println("Out10 is off");
+					} else {
+						Serial.println("Out10 is on");
+					}
+					break;
+			}
+		}
+	}
 }
 
 byte mapKey() {
@@ -88,17 +317,4 @@ byte mapKey() {
 	if (data == 0 ) key = 'D';
 	
 	return key;
-}
-
-int getState(int checkPin) {
-// Check the output state from each pin
-	int out;
-	out = digitalRead(checkPin);
-	return out;
-}
-
-void controller () {
-	//Enable or disable outputs
-	getState(9); //Get the output state of pin 9
-
 }
